623d6dbed89f34ada97196f72f60dc50dc3c5383
Rahul Das Kumar <Rahuldas2390@gmail.com>
2024-08-27 20:13:11 +0530

    Add files via upload

---
+++ DAY1_DAY2_first_file.py
@@ -0,0 +1,72 @@
+# print("HELLO")
+# print("hello world!")
+# this is a comment
+print("       ")
+print("Day 1- Python Print Function")
+print("The function is declared like this:")
+print("print('''what to print''')")
+print("Hello World!\nHello World!\nHello World\n ")
+print(" Hello World!\n Hello World!\n Hello World\n ")
+# print("Hello"+"Ojas") #OUTPUT:-HelloOjas
+# print("Hello"+" Ojas") #OUTPUT:-Hello Ojas
+print("Hello"+" "+"Ojas") #Hello Ojas
+# print("HELLo")
+# a=int(input("What is your age?"))
+# print("Your age is:", a)
+# name=input("What is your name?")
+# print("Your name is:"+name)
+# print("Hello "+input("What is your name?"))
+# print(len(input()))
+# len=len(input())
+# print("The length of the word is:",len)
+# name=input("What is your name?")
+# print(name)
+# name="OJAS"
+# print(name)
+# james=9922034132
+# print(james)
+# num1=int(input("Enter the value of num1:"))
+# num2=int(input("Enter the value of num2:"))
+# print("The value of num1 is:",num1)
+# print("The value of num2 is:",num2)
+# a=input()
+# b=input()
+# c=a
+# a=b
+# b=c
+# print("a:"+a)
+# print("b:"+b)
+# nama="JHACK"
+# print(nama)
+# print("Hello[0]")
+print("DAY 2 IN PYTHON")
+# print(123+234)
+# print(123+234)
+# number=str(1231231231231)
+# print(len(number))
+# ⁡⁢⁣⁢num_char=input("What is your name?")
+# print(type(num_char))
+# new_numb_char=str(num_char)
+# print("Your name has",len(new_numb_char),"characters")⁡
+# PEMDAS
+# Paranthesis
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Subtraction
+print("The value of 2 power 6 is:",2**6)
+print(3*3+3/3-7)
+print(round(2.6777777777777777777,1))
+print(8//3)
+score=0
+score+=1
+height=1.8
+isWinning=True
+name="Ojas"
+surname="Bidkar"
+currentClass="T.E"
+# print("Your score is:",score)
+print(f"Your score is {score},your height is {height} ,you are winning is {isWinning}")
+print(f"Name is {name},Surname is {surname} and currently studying in {currentClass}")
+
